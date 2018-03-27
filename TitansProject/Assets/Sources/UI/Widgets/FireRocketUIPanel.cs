8137ac59aeaecd42f9aaca3d13604485090690c9
k.kostromin <k.kostomin@corpwebgames.com>
2018-03-27 21:22:10 +0300

    Rockets fire and count interface

---
+++ TitansProject/Assets/Sources/UI/Widgets/FireRocketUIPanel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FireRocketUIPanel : MonoBehaviour {
+    [SerializeField]
+    private Button fireRocketButton; // Set from editor
+    [SerializeField]
+    private Text rocketsCount; // Set from editor
+
+    public void OnFireRocketClick() { // Set from editor
+        Debug.LogError("OnFireRocket");
+    }
+
+	// Use this for initialization
+	private void Start () {
+        fireRocketButton.onClick.RemoveAllListeners();
+        fireRocketButton.onClick.AddListener(OnFireRocketClick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
