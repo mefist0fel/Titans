17ce2c48731f728cdbb87c89446433bbbedc1378
DESKTOP-T51FB32\Mefistofel <Mfstfl@gmail.com>
2018-04-22 22:02:02 +0300

    Added touch scale up / down

--- TitansProject/Assets/Sources/Game/CameraController.cs
+++ TitansProject/Assets/Sources/Game/CameraController.cs
@@ -79,40 +79,94 @@ public sealed class CameraController : MonoBehaviour {
         SetLevel(levels[1]);
     }
  
-    Vector3 prevPosition;
+    Vector2 prevPosition;
     private void Update () {
-        if (Input.GetMouseButtonDown(0)) {
-            prevPosition = Input.mousePosition;
-        }
-        if (Input.GetMouseButton(0)) {
-            const float unitScaleFactor = 2000f;
-            var delta = (Input.mousePosition - prevPosition);
-            rotationInertionVelocity.x = delta.x / Screen.height * unitScaleFactor;
-            rotationInertionVelocity.y = delta.y / Screen.height * unitScaleFactor;
-            prevPosition = Input.mousePosition;
+        if (Input.touchSupported) {
+            if (Input.touchCount == 2) {
+                var touchA = Input.GetTouch(0);
+                var touchB = Input.GetTouch(1);
+                Vector2 prevTouchAPosition = touchA.position;
+                Vector2 prevTouchBPosition = touchB.position;
+                if (touchA.phase == TouchPhase.Moved) {
+                    prevTouchAPosition = touchA.position - touchA.deltaPosition;
+                }
+                if (touchB.phase == TouchPhase.Moved) {
+                    prevTouchBPosition = touchB.position - touchB.deltaPosition;
+                }
+
+                RotateByChangeDoubleTouchAngle(
+                    touchA.position,
+                    touchB.position,
+                    prevTouchAPosition,
+                    prevTouchBPosition);
+                ScaleByChangeDoubleTouchAngle(
+                    touchA.position,
+                    touchB.position,
+                    prevTouchAPosition,
+                    prevTouchBPosition);
+            }
+            if (Input.touchCount == 1) {
+                var touchA = Input.GetTouch(0);
+                prevPosition = touchA.position;
+                if (touchA.phase == TouchPhase.Moved) {
+                    prevPosition = touchA.position - touchA.deltaPosition;
+                }
+                const float unitScaleFactor = 30f;
+                var delta = (touchA.position - prevPosition);
+                rotationInertionVelocity.x = delta.x / Screen.height * unitScaleFactor;
+                rotationInertionVelocity.y = delta.y / Screen.height * unitScaleFactor;
+                prevPosition = touchA.position;
+            } else {
+                rotationInertionVelocity.Scale(Vector2.one - inertionAttenuation * Time.deltaTime);
+            }
         } else {
-            rotationInertionVelocity.Scale(Vector2.one - inertionAttenuation * Time.deltaTime);
+            if (Input.GetMouseButtonDown(0)) {
+                prevPosition = Input.mousePosition;
+            }
+            var ctrl = Input.GetKey(KeyCode.LeftControl);
+            if (Input.GetMouseButton(0)) {
+                if (ctrl) {
+                    RotateByChangeDoubleTouchAngle(
+                        new Vector2(Screen.width / 2, Screen.height / 2),
+                        Input.mousePosition,
+                        new Vector2(Screen.width / 2, Screen.height / 2),
+                        prevPosition);
+                    ScaleByChangeDoubleTouchAngle(
+                        new Vector2(Screen.width / 2, Screen.height / 2),
+                        Input.mousePosition,
+                        new Vector2(Screen.width / 2, Screen.height / 2),
+                        prevPosition);
+                } else {
+                    const float unitScaleFactor = 30f;
+                    var delta = ((Vector2)Input.mousePosition - prevPosition);
+                    rotationInertionVelocity.x = delta.x / Screen.height * unitScaleFactor;
+                    rotationInertionVelocity.y = delta.y / Screen.height * unitScaleFactor;
+                }
+                prevPosition = Input.mousePosition;
+            } else {
+                rotationInertionVelocity.Scale(Vector2.one - inertionAttenuation * Time.deltaTime);
+            }
         }
         Rotate(Vector3.forward, rotationInertionVelocity.x);
         Rotate(Vector3.left, rotationInertionVelocity.y);
 
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-            Rotate(Vector3.left, -AngularSpeed);
+            Rotate(Vector3.left, -AngularSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-            Rotate(Vector3.left, AngularSpeed);
+            Rotate(Vector3.left, AngularSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-            Rotate(Vector3.forward, AngularSpeed);
+            Rotate(Vector3.forward, AngularSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            Rotate(Vector3.forward, -AngularSpeed);
+            Rotate(Vector3.forward, -AngularSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.Q)) {
-            Rotate(Vector3.up, -rotationSpeed);
+            Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.E)) {
-            Rotate(Vector3.up, rotationSpeed);
+            Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
         }
         if (changeLevelTimer > 0) {
             changeLevelTimer -= Time.deltaTime;
@@ -139,6 +193,36 @@ public sealed class CameraController : MonoBehaviour {
         SetRotation();
     }
 
+    float prevDistance = 0;
+    private void ScaleByChangeDoubleTouchAngle(Vector2 positionA, Vector2 positionB, Vector2 prevPositionA, Vector2 prevPositionB) {
+        if (changeLevelTimer > 0)
+            return;
+        var distance = Vector2.Distance(positionA, positionB);
+        if (positionA == prevPositionA && positionB == prevPositionB) {
+            prevDistance = Vector2.Distance(prevPositionA, prevPositionB);
+        }
+        const float precision = 1.05f; // 5%
+        if (distance > prevDistance * precision) {
+            prevDistance = distance;
+            cameraLevel -= 1;
+        }
+        if (distance * precision < prevDistance) {
+            prevDistance = distance;
+            cameraLevel += 1;
+        }
+        if (cameraLevel < 0)
+            cameraLevel = 0;
+        if (cameraLevel >= levels.Length)
+            cameraLevel = levels.Length - 1;
+        SetLevel(levels[cameraLevel]);
+    }
+
+    private void RotateByChangeDoubleTouchAngle(Vector2 positionA, Vector2 positionB, Vector2 prevPositionA, Vector2 prevPositionB) {
+        var currentAngle = Utils.GetAngle(positionA, positionB);
+        var prevAngle = Utils.GetAngle(prevPositionA, prevPositionB);
+        Rotate(Vector3.up, prevAngle - currentAngle);
+    }
+
     private void SetLevel(VisualLevelData visualLevelData) {
         changeLevelTimer = timeToChangeLevel;
         needCameraDistance = visualLevelData.Distance;
@@ -152,6 +236,6 @@ public sealed class CameraController : MonoBehaviour {
     }
 
     private void Rotate(Vector3 axe, float angularSpeed) {
-        rotation = rotation * Quaternion.AngleAxis(angularSpeed * Time.deltaTime, axe);
+        rotation = rotation * Quaternion.AngleAxis(angularSpeed, axe);
     }
 }
