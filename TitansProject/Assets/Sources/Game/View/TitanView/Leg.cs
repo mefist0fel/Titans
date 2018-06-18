eef5ec4fbdd0107ba0100d0983dede960a8d0b45
DESKTOP-T51FB32\Mefistofel <Mfstfl@gmail.com>
2018-06-19 01:07:41 +0300

    Legs prototype

---
+++ TitansProject/Assets/Sources/Game/View/TitanView/Leg.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class Leg : MonoBehaviour {
+    [SerializeField]
+    private float fullTime = 0.08f;
+    [SerializeField]
+    private float correctionTime = 0.06f;
+
+    private Vector3 currentAbsolutePosition;
+    private Vector3 prevAbsolutePosition;
+    private float timer;
+    private Vector3 prevPosition;
+    private Vector3 correctionVector;
+
+    private void Start () {
+        currentAbsolutePosition = transform.position;
+        prevPosition = transform.position;
+    }
+	
+	private void Update () {
+        UpdateCorrection();
+        if (timer > 0) {
+            timer -= Time.deltaTime;
+            var normalizedTime = Mathf.Clamp01(timer / fullTime);
+            currentAbsolutePosition = Vector3.Lerp(transform.position + correctionVector, prevAbsolutePosition, normalizedTime);
+        }
+    }
+
+    private void UpdateCorrection() {
+        correctionVector = (transform.position - prevPosition) * (1f / Time.deltaTime * correctionTime);
+        prevPosition = transform.position;
+    }
+
+    public void StartStep() {
+        timer = fullTime;
+        prevAbsolutePosition = currentAbsolutePosition;
+    }
+
+    public void StopStep() {}
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(currentAbsolutePosition, 0.1f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position + correctionVector, 0.06f);
+    }
+}
---
+++ TitansProject/Assets/Sources/Game/View/TitanView/LegsController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Component controls legs steps order
+/// </summary>
+public sealed class LegsController : MonoBehaviour {
+    [SerializeField]
+    private Leg[] legs; // Set from editor
+    [SerializeField]
+    private float stepTime = 0.1f;
+
+    private float timer;
+    private int currentLegId;
+
+	private void Start () {
+		
+	}
+	
+	private void Update () {
+        timer -= Time.deltaTime;
+        if (timer < 0) {
+            MoveNextLeg();
+            timer = stepTime;
+        }
+	}
+
+    private void MoveNextLeg() {
+        var currentLeg = legs[currentLegId];
+        currentLeg.StopStep();
+        currentLegId = (currentLegId + 1) % legs.Length;
+        var nextLeg = legs[currentLegId];
+        nextLeg.StartStep();
+    }
+}
