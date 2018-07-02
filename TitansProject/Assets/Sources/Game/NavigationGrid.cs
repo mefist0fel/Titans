14bffa544ded3e5077adc6cde722fdd353874093
DESKTOP-T51FB32\Mefistofel <Mfstfl@gmail.com>
2018-07-03 00:18:25 +0300

    All sides nav grid

--- TitansProject/Assets/Sources/Game/NavigationGrid.cs
+++ TitansProject/Assets/Sources/Game/NavigationGrid.cs
@@ -14,18 +14,29 @@ public sealed class NavigationGrid : MonoBehaviour {
 
     private NavigationPoint[] points;
 
+    [ContextMenu("Regenerate grid")]
 	private void Start () {
         points = GenerateNavGrid(radius);
 	}
 
     private NavigationPoint[] GenerateNavGrid(float radius) {
         List<NavigationPoint> grid = new List<NavigationPoint>();
+        var directions = new Quaternion[] {
+            Quaternion.Euler(0, 0, 0),
+            Quaternion.Euler(0, 90, 0),
+            Quaternion.Euler(0, 180, 0),
+            Quaternion.Euler(0, 270, 0),
+            Quaternion.Euler(90, 0, 0),
+            Quaternion.Euler(-90, 0, 0),
+        };
         // top
         const int density = 20;
         for (int i = 0; i < density; i++) {
             for (int j = 0; j < density; j++) {
-                Vector3 normal = new Vector3(-1 + 2f * (i / (float)density), -1 + 2f * (j / (float)density), 1).normalized;
-                grid.Add(new NavigationPoint() { Position = normal * radius, UpNormal = normal });
+                foreach (var direction in directions) {
+                    Vector3 normal = direction * new Vector3(-1 + 2f * ((i + 0.5f) / (float)density), -1 + 2f * ((j + 0.5f) / (float)density), 1).normalized;
+                    grid.Add(new NavigationPoint() { Position = normal * radius, UpNormal = normal });
+                }
             }
         }
         return grid.ToArray();
