3c888096000fe8cde175d2800ecb3ebf50921b5d
mefistofel <mfstfl@gmail.com>
2018-07-08 16:48:28 +0300

    Navigation grid based on icosaedron

--- TitansProject/Assets/Sources/Game/NavigationGrid.cs
+++ TitansProject/Assets/Sources/Game/NavigationGrid.cs
@@ -36,28 +36,28 @@ public sealed class NavigationGrid : MonoBehaviour {
 
 
     private int[] icosaedronFaces = new int[] {
-        1, 2, 10,
-        1, 11, 2,
-        2, 5, 6,
-        3, 6, 5,
-        3, 4, 9,
-        3, 12, 4,
-        4, 8, 7,
-        1, 7, 8,
-        6, 9, 10,
-        7, 10, 9,
-        8, 12, 11,
-        5, 11, 12,
+        0, 1, 9,
+        0, 10, 1,
+        1, 4, 5,
+        2, 5, 4,
+        2, 3, 8,
+        2, 11, 3,
+        3, 7, 6,
+        0, 6, 7,
+        5, 8, 9,
+        6, 9, 8,
+        7, 11, 10,
+        4, 10, 11,
 
-        2, 6, 10,
-        1, 10, 7,
-        1, 8, 11,
-        2, 11, 5,
+        1, 5, 9,
+        0, 9, 6,
+        0, 7, 10,
+        1, 10, 4,
 
-        3, 9, 6,
-        4, 7, 9,
-        4, 12, 8,
-        3, 12, 5
+        2, 8, 5,
+        3, 6, 8,
+        3, 11, 7,
+        2, 11, 4
     };
 
     private sealed class NavigationBuildPoint {
@@ -80,7 +80,7 @@ public sealed class NavigationGrid : MonoBehaviour {
 
     [ContextMenu("Regenerate grid")]
 	private void Start () {
-        points = GenerateNavGrid(radius);
+        points = GenerateNavGridIcosaedron(radius);
         graph = new NavigationGraph(FinalizeGraph(points));
 
     }
@@ -97,6 +97,92 @@ public sealed class NavigationGrid : MonoBehaviour {
             };
         }
         return nodes;
+    }
+    private NavigationBuildPoint[] GenerateNavGridIcosaedron(float radius) {
+        List<NavigationBuildPoint> points = new List<NavigationBuildPoint>();
+        List<NavigationBuildPoint> borderPoins = new List<NavigationBuildPoint>();
+        NavigationBuildPoint[] grid = new NavigationBuildPoint[size * size];
+        for (int d = 0; d < icosaedronFaces.Length / 3; d++) {
+            var A = icosaedronVertices[icosaedronFaces[d * 3 + 0]];
+            var B = icosaedronVertices[icosaedronFaces[d * 3 + 1]];
+            var C = icosaedronVertices[icosaedronFaces[d * 3 + 2]];
+            var BA = B - A;
+            var CA = C - A;
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (i + j >= size)
+                        continue;
+                    Vector3 cellPosition = A + BA * (i / (float)(size - 1)) + CA * (j / (float)(size - 1));
+                    Vector3 normal = cellPosition.normalized;
+                    Vector3 cell = (normal + Vector3.one) * size * 10f;
+                    bool isBorderPoint = (i == 0 || j == 0 || i + j == size - 1);
+                    var point = new NavigationBuildPoint() {
+                        Position = normal * radius,
+                        UpNormal = normal,
+                        IsBorderPoint = isBorderPoint,
+                        PositionHash = (int)(cell.x * (size + 1f) * (size + 1f)) + (int)(cell.y * (size + 1f)) + (int)(cell.z)
+                    };
+                    if (isBorderPoint)
+                        borderPoins.Add(point);
+                    points.Add(point);
+                    grid[i * size + j] = point;
+                }
+            }
+            // add links
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (i + j >= size)
+                        continue;
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i + 1, j);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i - 1, j);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i, j + 1);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i, j - 1);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i - 1, j + 1);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i + 1, j - 1);
+
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i + 1, j + 1);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i - 1, j - 1);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i - 1, j + 2);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i - 2, j + 1);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i + 2, j - 1);
+                    AddLinksHex(ref grid, ref grid[i * size + j].Neigbhors, i + 1, j - 2);
+                }
+            }
+        }
+        // merge borders
+        foreach (var point in borderPoins) {
+            if (point.Neigbhors == null)
+                continue;
+            foreach (var merge in borderPoins) {
+                if (merge == point)
+                    continue;
+                if (merge.Neigbhors == null)
+                    continue;
+                if (merge.PositionHash == point.PositionHash) {
+                    MergePoints(point, merge);
+                    break;
+                }
+            }
+        }
+        points.RemoveAll(point => point.Neigbhors == null);
+        foreach (var point in points) {
+            point.Distances = new List<float>(point.Neigbhors.Count);
+            for (int i = 0; i < point.Neigbhors.Count; i++) {
+                var distance = Vector3.Distance(point.Position, point.Neigbhors[i].Position);
+                point.Distances.Add(distance);
+            }
+        }
+        return points.ToArray();
+    }
+
+    private void AddLinksHex(ref NavigationBuildPoint[] grid, ref List<NavigationBuildPoint> neigbhors, int x, int y) {
+        if (x < 0)
+            return;
+        if (y < 0)
+            return;
+        if (x + y >= size)
+            return;
+        neigbhors.Add(grid[x * size + y]);
     }
 
     private NavigationBuildPoint[] GenerateNavGrid(float radius) {
@@ -142,14 +228,6 @@ public sealed class NavigationGrid : MonoBehaviour {
                             }
                         }
                     }
-                    // AddLinks(ref grid, ref grid[i * size + j].Neigbhors, i + 1, j    );
-                    // AddLinks(ref grid, ref grid[i * size + j].Neigbhors, i + 1, j + 1);
-                    // AddLinks(ref grid, ref grid[i * size + j].Neigbhors, i    , j + 1);
-                    // AddLinks(ref grid, ref grid[i * size + j].Neigbhors, i - 1, j + 1);
-                    // AddLinks(ref grid, ref grid[i * size + j].Neigbhors, i - 1, j);
-                    // AddLinks(ref grid, ref grid[i * size + j].Neigbhors, i - 1, j - 1);
-                    // AddLinks(ref grid, ref grid[i * size + j].Neigbhors, i    , j - 1);
-                    // AddLinks(ref grid, ref grid[i * size + j].Neigbhors, i + 1, j - 1);
                 }
             }
         }
@@ -286,28 +364,36 @@ public sealed class NavigationGrid : MonoBehaviour {
     private bool showPath = true;
     [SerializeField]
     private bool showLines = true;
+    [SerializeField]
+    private bool showIcosaedron = true;
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.red;
-        foreach (var point in icosaedronVertices) {
-            Gizmos.DrawWireSphere(point * 11, 0.1f);
+        const float visualSize = 11f;
+        if (showIcosaedron) {
+            foreach (var point in icosaedronVertices) {
+                Gizmos.DrawWireSphere(point * visualSize, 0.1f);
+            }
+            for (int i = 0; i < 20; i++) {
+                var a = icosaedronVertices[icosaedronFaces[i * 3 + 0]] * visualSize;
+                var b = icosaedronVertices[icosaedronFaces[i * 3 + 1]] * visualSize;
+                var c = icosaedronVertices[icosaedronFaces[i * 3 + 2]] * visualSize;
+                var center = (a + b + c) * (1f / 3f);
+                a = center + (a - center) * 0.9f;
+                b = center + (b - center) * 0.9f;
+                c = center + (c - center) * 0.9f;
+                Gizmos.DrawLine(a, b);
+                Gizmos.DrawLine(b, c);
+                Gizmos.DrawLine(c, a);
+            }
         }
-        for (int i = 0; i < 20; i++) {
-            var a = icosaedronVertices[icosaedronFaces[i * 3 + 0] - 1] * 11;
-            var b = icosaedronVertices[icosaedronFaces[i * 3 + 1] - 1] * 11;
-            var c = icosaedronVertices[icosaedronFaces[i * 3 + 2] - 1] * 11;
-            var center = (a + b + c) * (1f / 3f);
-            a = center + (a - center) * 0.9f;
-            b = center + (b - center) * 0.9f;
-            c = center + (c - center) * 0.9f;
-            Gizmos.DrawLine(a, b);
-            Gizmos.DrawLine(b, c);
-            Gizmos.DrawLine(c, a);
-        }
-        Gizmos.color = Color.blue;
         if (points != null) {
             foreach (var point in points) {
+                Gizmos.color = point.IsBorderPoint ? Color.yellow : Color.blue;
                 Gizmos.DrawWireSphere(point.Position, 0.02f);
+            }
+            Gizmos.color = Color.blue;
+            foreach (var point in points) {
                 if (point.Neigbhors != null && showLines) {
                     foreach (var neigbhor in point.Neigbhors) {
                         Gizmos.DrawLine(point.Position, neigbhor.Position);
